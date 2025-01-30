296160c4e93c8970d76a5a3a5d0b10fc67e407fd
Hudson Stohl <hrstohl02@gmail.com>
2025-01-30 09:27:57 -0700

    added basic tests

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -4,11 +4,37 @@ const app = require('../service');
 const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
 let testUserAuthToken;
 
+if (process.env.VSCODE_INSPECTOR_OPTIONS) {
+    jest.setTimeout(60 * 1000 * 5); // 5 minutes
+}
+
 beforeAll(async () => {
   testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
   const registerRes = await request(app).post('/api/auth').send(testUser);
   testUserAuthToken = registerRes.body.token;
 });
+
+test('register', async () => {
+    const user = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
+    user.email = Math.random().toString(36).substring(2, 12) + '@test.com';
+    const regRes = await request(app).post('/api/auth').send(user);
+    expect(regRes.status).toBe(200);
+    // expect(lregRes.body.token).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
+  
+    // const { password, ...user } = { ...testUser, roles: [{ role: 'diner' }] };
+    // expect(loginRes.body.user).toMatchObject(user);
+  });
+
+  test('register without password', async () => {
+    const user = { name: 'pizza diner', email: 'reg@test.com'};
+    user.email = Math.random().toString(36).substring(2, 12) + '@test.com';
+    const regRes = await request(app).post('/api/auth').send(user);
+    expect(regRes.status).toBe(400);
+    // expect(lregRes.body.token).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
+  
+    // const { password, ...myUser } = { ...user, roles: [{ role: 'diner' }] };
+    // expect(regRes.body.user).toMatchObject(myUser);
+  });
 
 test('login', async () => {
   const loginRes = await request(app).put('/api/auth').send(testUser);
