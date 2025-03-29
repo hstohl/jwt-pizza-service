930550c748d6d5b4c260a1551d69e9d3ead06263
Hudson Stohl <hrstohl02@gmail.com>
2025-03-29 12:19:32 -0600

    finished logging for real

--- src/endpointHelper.js
+++ src/endpointHelper.js
@@ -1,6 +1,9 @@
+const logger = require("./logger");
+
 class StatusCodeError extends Error {
   constructor(message, statusCode) {
     super(message);
+    logger.log("error", "unhandled error", this);
     this.statusCode = statusCode;
   }
 }
