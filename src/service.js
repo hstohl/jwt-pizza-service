c4103d7336c93241d30ee45c5e17f8f7ffa4c7da
Hudson Stohl <hrstohl02@gmail.com>
2025-04-01 03:43:59 -0600

    cors fix? v2

--- src/service.js
+++ src/service.js
@@ -9,19 +9,21 @@ const logger = require("./logger");
 const cors = require("cors");
 
 const app = express();
+const corsOptions = {
+  origin: "https://pizza.hudson-stohl.click",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions)); // Move this to the top before other middleware
+
+// Explicitly handle preflight requests
+app.options("*", cors(corsOptions));
+
 app.use(express.json());
 app.use(setAuthUser);
-app.use(cors({ origin: "https://pizza.hudson-stohl.click" }));
-
 app.use(logger.httpLogger);
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", req.headers.origin || "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  next();
-});
 
 const apiRouter = express.Router();
 app.use("/api", apiRouter);
