6bc5c04859e3ba1f6ff7c690718218816fe4787d
Hudson Stohl <hrstohl02@gmail.com>
2025-04-01 08:35:46 -0600

    removed cors for purity before talking to TA

--- src/service.js
+++ src/service.js
@@ -6,10 +6,10 @@ const franchiseRouter = require("./routes/franchiseRouter.js");
 const version = require("./version.json");
 const config = require("./config.js");
 const logger = require("./logger");
-const cors = require("cors");
+//const cors = require("cors");
 
 const app = express();
-app.use(cors({ origin: "https://pizza.hudson-stohl.click" }));
+//app.use(cors({ origin: "https://pizza.hudson-stohl.click" }));
 app.use(express.json());
 app.use(setAuthUser);
 
