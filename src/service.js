b5b6507b96dbe7d7e05a72e6bd50ceb220633df6
Hudson Stohl <hrstohl02@gmail.com>
2025-04-01 17:56:36 -0600

    cors fix attempt

--- src/service.js
+++ src/service.js
@@ -19,9 +19,17 @@ app.use(logger.httpLogger);
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", req.headers.origin || "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Credentials", "true");
+
+  if (req.method === "OPTIONS") {
+    return res.status(204).end();
+  }
+
   next();
 });
 
