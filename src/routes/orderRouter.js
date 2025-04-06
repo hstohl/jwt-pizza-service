0b06fe386a324734de63b919ac0e5f391078f7b3
Hudson Stohl <hrstohl02@gmail.com>
2025-04-05 23:30:26 -0600

    added chaos monkey

--- src/routes/orderRouter.js
+++ src/routes/orderRouter.js
@@ -105,6 +105,27 @@ orderRouter.put(
   })
 );
 
+// Chaos Monkey
+let enableChaos = false;
+orderRouter.put(
+  "/chaos/:state",
+  authRouter.authenticateToken,
+  asyncHandler(async (req, res) => {
+    if (req.user.isRole(Role.Admin)) {
+      enableChaos = req.params.state === "true";
+    }
+
+    res.json({ chaos: enableChaos });
+  })
+);
+
+orderRouter.post("/", (req, res, next) => {
+  if (enableChaos && Math.random() < 0.5) {
+    throw new StatusCodeError("Chaos monkey", 500);
+  }
+  next();
+});
+
 // getOrders
 orderRouter.get(
   "/",
